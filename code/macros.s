3a295cb1dbd52be620615aef15e0f3ca9ed44706
Robert Winkler <rob121618@gmail.com>
2021-06-09 15:30:30 -0700

    Adding about macros

---
+++ code/macros.s
@@ -0,0 +1,52 @@
+
+.data
+
+.macro sys_exit
+li   $v0, 10
+syscall
+.end_macro
+
+.macro print_int_reg(%x)
+li    $v0, 1
+move  $a0, %x
+syscall
+.end_macro
+
+.macro print_str_label(%x)
+li     $v0, 4
+la     $a0, %x
+syscall
+.end_macro
+
+.macro print_str(%str)
+.data
+str: .asciiz %str
+.text
+li     $v0, 4
+la     $a0, str
+syscall
+.end_macro
+
+
+
+str1:   .asciiz "Hello 1\n"
+
+
+
+.text
+
+main:
+
+	print_str_label(str1)
+
+	print_str("Hello World!\n")
+
+	li   $t0, 42
+	print_int_reg($t0)
+
+
+
+
+	#sys_exit
+	li   $v0, 10
+	syscall
