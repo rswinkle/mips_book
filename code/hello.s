48ddef0230004f18a93866bceb82f3d284e9707f
Robert Winkler <rob121618@gmail.com>
2023-01-01 13:28:54 -0700

    Minor tweaks to ch0

--- code/hello.s
+++ code/hello.s
@@ -3,7 +3,7 @@ hello:   .asciiz "Hello World!\n"
 
 .text
 main:
-	li   $v0, 4      # print string system call
+	li   $v0, 4      # load immediate, v0 = 4 (4 is print string system call)
 	la   $a0, hello  # load address of string to print into a0
 	syscall
 
